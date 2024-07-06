605dc172d63b80d483962f3b42d493cd35c0602d
JLisenbee <jakelisenbee@gmail.com>
2024-07-06 11:51:03 -0400

    Added SpringBoot Java/SQLite Project

---
+++ PersonalProjects/MechDB with Java/src/main/java/com/sqlite/mechdb/MechdbApplication.java
@@ -0,0 +1,13 @@
+package com.sqlite.mechdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MechdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MechdbApplication.class, args);
+	}
+
+}
---
+++ PersonalProjects/MechDB with Java/src/test/java/com/sqlite/mechdb/MechdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqlite.mechdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MechdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
